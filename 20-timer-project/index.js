0cec1b2dd6ac58dc3a3000d159aa4258947374ff
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 03:34:13 -0400

    comments

--- 20-timer-project/index.js
+++ 20-timer-project/index.js
@@ -1,15 +1,17 @@
-// Query Selects
+//* Select DOM elements
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
+
+//* Set perimeter and stroke-dasharray
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
 let duration;
-
-//* Create New Timer
+//* New instance of Timer, fourth argument (optional callbacks)
 const timer = new Timer(durationInput, startButton, pauseButton, {
+  //* Hook Timer to the real world
   onStart(totalDuration) {
     duration = totalDuration;
   },
@@ -20,6 +22,6 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     );
   },
   onComplete() {
-    console.log('Timer Complete');
+    console.log('Timer is completed');
   },
 });
--- 20-timer-project/timer.js
+++ 20-timer-project/timer.js
@@ -1,24 +1,23 @@
 class Timer {
+  //* Constructor takes DOM elements
   constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    // Optional Callbacks
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
     }
 
-    // When buttons are click, start or pause are called
+    //* Event Listeners
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
 
-  //* Start Timer
+  //* Starts the timer
   start = () => {
-    // Check if onComplete was called
     if (this.onStart) {
       this.onStart(this.timeRemaining);
     }
@@ -27,38 +26,32 @@ class Timer {
     this.interval = setInterval(this.tick, 20);
   };
 
-  // Pause Timer
+  //* Pauses the interval
   pause = () => {
     clearInterval(this.interval);
   };
 
-  // Countdown Timer
+  //* Responsible for the timer counting down
   tick = () => {
-    // Stops the timer at 0
     if (this.timeRemaining <= 0) {
       this.pause();
-
-      // Check if onComplete was called
       if (this.onComplete) {
         this.onComplete();
       }
     } else {
-      // this.timeRemaining(this.timeRemaining - 0.02)
       this.timeRemaining = this.timeRemaining - 0.02;
-
-      // Check if onComplete was called
       if (this.onTick) {
         this.onTick(this.timeRemaining);
       }
     }
   };
 
-  // Retrieve a variable from class
+  //* Instance variable to get input value
   get timeRemaining() {
     return parseFloat(this.durationInput.value);
   }
 
-  // Set the value
+  //* Instance variable to set input value
   set timeRemaining(time) {
     this.durationInput.value = time.toFixed(2);
   }
