1e1b8406d29eb0a7861c060a7720cff8698771d9
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 22:41:18 -0400

    comments

--- 24-node-js-cli-tools-project/index.js
+++ 24-node-js-cli-tools-project/index.js
@@ -1,30 +1,39 @@
 #!/usr/bin/env node
+//* ^-- Node to execute file
+// Developer Reminder -> npm link -> link filename
+
+//* File System From Node
 const fs = require('fs');
 const util = require('util');
-const chalk = require('chalk');
 const path = require('path');
 
-// Method #2
+const chalk = require('chalk');
+
+// Method #2 with Promise
 // const lstat = util.promisify(fs.lstat);
 
-// Method #3
+//* Method #3 with Promise
 const { lstat } = fs.promises;
 
+//* Sets directory to argument after nls or current directory
 const targetDir = process.argv[2] || process.cwd();
 
-//* Read the directory
+//* Read Current Directory
 fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
+  // Get information about file and determine type with lstat
   const statPromises = filenames.map((filename) => {
     return lstat(path.join(targetDir, filename));
   });
 
+  // Wait for all the promises to be finish
   const allStats = await Promise.all(statPromises);
 
   for (let stats of allStats) {
+    // stats does not have info about file, need index
     const index = allStats.indexOf(stats);
 
     if (stats.isFile()) {
@@ -35,15 +44,15 @@ fs.readdir(targetDir, async (err, filenames) => {
   }
 });
 
-// Method #1
+// Method #1 with Promise
 // const lstat = (filename) => {
-//     return new Promise((resolve, reject) => {
-//         fs.lstat(filename, (err, stats) => {
-//             if (err) {
-//                 reject(err);
-//             }
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
 
-//             resolve(stats);
-//         });
+//       resolve(stats);
 //     });
+//   });
 // };
